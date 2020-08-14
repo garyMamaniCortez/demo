db95d38c91ae7248e1f00b2b756885ccfcbd6f42
garyMamaniCortez <garymamani12@gmail.com>
2020-08-14 18:47:32 -0400

    .

--- src/main/java/org/scesi/demo/repository/SubjectsRepository.java
+++ src/main/java/org/scesi/demo/repository/SubjectsRepository.java
@@ -1,4 +1,4 @@
-package org.scesi.example.clases;
+package org.scesi.demo.clases;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
