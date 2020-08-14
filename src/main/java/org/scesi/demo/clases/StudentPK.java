8b3c6e177697233243aa55a252053e1e4ae76466
garyMamaniCortez <garymamani12@gmail.com>
2020-08-14 18:28:26 -0400

    primary keys created

---
+++ src/main/java/org/scesi/demo/clases/StudentPK.java
@@ -0,0 +1,11 @@
+package org.scesi.demo.clases;
+
+public class StudentPK{
+    private String correo;
+    private String username;
+    public StudentPK(String correo,String username){
+        super();
+        this.correo=correo;
+        this.username=username;
+    }
+}
---
+++ src/main/java/org/scesi/demo/clases/SubjectsPK.java
@@ -0,0 +1,11 @@
+package org.scesi.demo.clases;
+
+public class SubjectsPK{
+    private String cod_cls;
+    private String group_of;
+    public StudentPK(String cod_cls,String group_of){
+        super();
+        this.cod_cls=cod_cls;
+        this.group_of=group_of;
+    }
+}
